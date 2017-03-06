6127e40e1d6d1193e0df4ad15f7ba10de57f466b
Kamil <kamil@altel.pl>
2017-03-06 15:30:09 +0100

    Startowy

---
+++ PdfParsingIFilter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IFilter;
+
+namespace PdfParsingIFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // remember to rename the output exe to "filtdump.exe" to get access to the Adobe PDF IFilter 
+            // installed with Acrobat Reader
+
+            // read more: http://www.squarepdf.net/parsing-pdf-files-using-ifilter-c-net
+
+            string path = "circ92.pdf";
+            Console.WriteLine("Parsing " + path);
+            Console.WriteLine(ExtractTextFromPdf(path));
+        }
+
+        public static string ExtractTextFromPdf(string path)
+        {
+			// DefaultParser.
+
+			return DefaultParser.Extract(path);
+        }
+    }
+}
